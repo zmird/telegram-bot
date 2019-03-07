acd3b606a3e5a87e9474ab052f9afa8b748951a3
zakaria <mird.zakaria@gmail.com>
2019-03-07 19:15:33 +0100

    Added csv files.

--- settings.py
+++ settings.py
@@ -5,11 +5,8 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
-LOG_LEVEL_DEBUG = True
 TOKEN = config('TOKEN')
 
 APPLICATIONS = [
     "food"
 ]
-
-RESTAURANT_DEFAULT = ""
